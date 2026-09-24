b0fa669608385dfdd45c012a11b736ef1279ff27
atakan6195 <100753501+atakan6195@users.noreply.github.com>
2026-09-24 14:18:47 +0300

    Bump version to 1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
  namespace="com.atakan.altintakip"; compileSdk=35
- defaultConfig { applicationId="com.atakan.altintakip"; minSdk=26; targetSdk=35; versionCode=8; versionName="1.8" }
+ defaultConfig { applicationId="com.atakan.altintakip"; minSdk=26; targetSdk=35; versionCode=9; versionName="1.9" }
  compileOptions {
   sourceCompatibility = JavaVersion.VERSION_17
   targetCompatibility = JavaVersion.VERSION_17
