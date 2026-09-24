8706afa5f5e708137a91eddff74533ad14f2d845
atakan6195 <100753501+atakan6195@users.noreply.github.com>
2026-09-24 13:55:28 +0300

    Bump version to 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
  namespace="com.atakan.altintakip"; compileSdk=35
- defaultConfig { applicationId="com.atakan.altintakip"; minSdk=26; targetSdk=35; versionCode=5; versionName="1.5" }
+ defaultConfig { applicationId="com.atakan.altintakip"; minSdk=26; targetSdk=35; versionCode=6; versionName="1.6" }
  compileOptions {
   sourceCompatibility = JavaVersion.VERSION_17
   targetCompatibility = JavaVersion.VERSION_17
